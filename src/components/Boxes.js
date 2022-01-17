2c5691f02c7160ca925933ac26af8d0b83a52029
Elise Beall <elisebeall@gmail.com>
2022-01-17 03:29:44 -0700

    Refactor to use Context

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,8 +1,11 @@
 import '../css/Boxes.css';
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
+import { CraftContext } from '../contexts/CraftContext';
 import SingleBox from './SingleBox';
 
-const Boxes = ({ spacecraft }) => {
+const Boxes = () => {
+  const { spacecraft } = useContext(CraftContext);
+
   return (
     <>
       {spacecraft.map(craft => <SingleBox craft={craft}/>)}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,27 @@
 import '../css/Search.css';
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useContext } from 'react';
+import { CraftContext } from '../contexts/CraftContext';
 import { FaSearch } from 'react-icons/fa';
 
 const Search = () => {
   const [query, setQuery] = useState('');
-  
+  const { spacecraft, setSpacecraft } = useContext(CraftContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSpacecraft(query);
+  }
+
   return (
-    <>
-      <FaSearch />
+    <form onSubmit={e => handleSubmit}>
+      <FaSearch className="icon-small" aria-hidden="true" />
       <input
         type="text"
         placeholder="search..."
         value={query}
-        onChange={setQuery}
+        onChange={e => setQuery(e.target.value)}
       />
-    </>
+    </form>
   )
 }
 
