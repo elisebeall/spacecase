c436eb724d71b8189a0da6dc8be3ae224c79b1d0
Elise Beall <elisebeall@gmail.com>
2022-01-17 03:25:58 -0700

    Refactor useFetch hook to use async & await

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,16 +5,41 @@ const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then(response => {
+  //       if(!response.ok) {
+  //         throw new Error('💥 CRASH 💥.  Sorry, please try that again...');
+  //       } return response.json();
+  //     })
+  //     .then(data => {
+  //       setData(data);
+  //       setError(null);
+  //       setIsLoading(false);
+  //     })
+  //     .catch(error => {
+  //       setError(error.message);
+  //       setIsLoading(false);
+  //     })
+  // }, [url])
+
   useEffect(() => {
-    fetch(url)
-      .then(response => {
-        if(!response.ok) {
-          throw new Error('💥 CRASH 💥.  Sorry, please try that again...');
-        } return response.json();
-      })
+    const getData = async () => {
+      const response = await fetch(url);
+
+      if (response.status !== 200) {
+        throw new Error(`${response.status} : Cannot fetch the data.`)
+      } else if (!response.ok) {
+        throw new Error ('💥 CRASH 💥.  Sorry, please try that again...');
+      }
+
+      const data = await response.json();
+
+      return data;
+    }
+    getData()
       .then(data => {
         setData(data);
-        setError(null);
         setIsLoading(false);
       })
       .catch(error => {
