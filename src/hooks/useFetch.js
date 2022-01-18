d8ade7ef45f3ae1042da0e72b64219c6bf5f9df6
Elise Beall <elisebeall@gmail.com>
2022-01-17 20:07:05 -0700

    Add setTimeout hook to useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,47 +5,33 @@ const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  // useEffect(() => {
-  //   fetch(url)
-  //     .then(response => {
-  //       if(!response.ok) {
-  //         throw new Error('💥 CRASH 💥.  Sorry, please try that again...');
-  //       } return response.json();
-  //     })
-  //     .then(data => {
-  //       setData(data);
-  //       setError(null);
-  //       setIsLoading(false);
-  //     })
-  //     .catch(error => {
-  //       setError(error.message);
-  //       setIsLoading(false);
-  //     })
-  // }, [url])
-
   useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(url);
+    setTimeout(() => {
+      const getData = async () => {
+        const response = await fetch(url);
 
-      if (response.status !== 200) {
-        throw new Error(`${response.status} : Cannot fetch the data.`)
-      } else if (!response.ok) {
-        throw new Error ('💥 CRASH 💥.  Sorry, please try that again...');
+        if (response.status !== 200) {
+          throw new Error(`${response.status} : Cannot fetch the data.`)
+        } else if (!response.ok) {
+          throw new Error ('💥 CRASH 💥.  Sorry, please try that again...');
+        }
+
+        const data = await response.json();
+
+        return data;
       }
 
-      const data = await response.json();
-
-      return data;
-    }
-    getData()
-      .then(data => {
-        setData(data);
-        setIsLoading(false);
-      })
-      .catch(error => {
-        setError(error.message);
-        setIsLoading(false);
-      })
+      getData()
+        .then(data => {
+          setData(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch(error => {
+          setError(error.message);
+          setIsLoading(false);
+        })
+    }, 1000)
   }, [url])
 
   return { data, isLoading, error };
