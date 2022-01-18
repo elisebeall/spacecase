7987ef129a98cc6e53c9033cbd48188ee44ff61b
Elise Beall <elisebeall@gmail.com>
2022-01-17 21:18:29 -0700

    Add search methods to context providers

--- src/contexts/CraftContext.js
+++ src/contexts/CraftContext.js
@@ -4,10 +4,19 @@ export const CraftContext = createContext();
 
 const CraftContextProvider = (props) => {
   const [spacecraft, setSpacecraft] = useState([]);
+  const [filteredSpacecraft, setFilteredSpacecraft] = useState([]);
+
+  const searchCrafts = (query) => {
+    let filteringCrafts = filteredSpacecraft.filter(craft => {
+      return craft.name.toLowerCase().includes(query.toLowerCase());
+    });
+    setFilteredSpacecraft(filteringCrafts);
+  }
 
   return (
-    <CraftContext.Provider value={{spacecraft, setSpacecraft}}>
-      { props.children }
+    <CraftContext.Provider
+      value={{spacecraft, setSpacecraft, filteredSpacecraft, setFilteredSpacecraft, searchCrafts}}>
+        { props.children }
     </CraftContext.Provider>
   )
 }
--- src/contexts/LaunchersContext.js
+++ src/contexts/LaunchersContext.js
@@ -4,10 +4,19 @@ export const LaunchersContext = createContext();
 
 const LaunchersContextProvider = (props) => {
   const [launchers, setLaunchers] = useState([]);
-  
+  const [filteredLaunchers, setFilteredLaunchers] = useState([]);
+
+  const searchLaunchers = (query) => {
+    let filteringLaunchers = filteredLaunchers.filter(craft => {
+      return craft.name.toLowerCase().includes(query.toLowerCase());
+    });
+    setFilteredLaunchers(filteringLaunchers);
+  }
+
   return (
-    <LaunchersContext.Provider value={{launchers, setLaunchers}}>
-      { props.children }
+    <LaunchersContext.Provider
+      value={{launchers, setLaunchers, filteredLaunchers, searchLaunchers}}>
+        { props.children }
     </LaunchersContext.Provider>
   )
 }
