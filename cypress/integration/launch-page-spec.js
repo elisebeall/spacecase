8bb781484f7bfe2ed80ebfddd9fba7b5c56b91cf
Elise Beall <elisebeall@gmail.com>
2022-01-18 14:38:38 -0700

    Complete tests for launch & main page

--- cypress/integration/launch-page-spec.js
+++ cypress/integration/launch-page-spec.js
@@ -1,6 +1,13 @@
 describe('spacecase launch page flow', () => {
   it('should show a title, description, and navigation into the site', () => {
-
+    cy.visit('localhost:3000')
+      .get('h1').should('contain', 'spacecase')
   });
 
+  it('should show a 404 message if an incorrect URL path is entered', () => {
+    cy.visit('localhost:3000/potatoe')
+      .get('h2').should('contain', 'Whoa, you made a wrong turn. Please try again.')
+      .get('.icon-small').should('be.visible').click()
+      .url().should('include', '/')
+  });
 })
--- cypress/integration/main-page-spec.js
+++ cypress/integration/main-page-spec.js
@@ -1,6 +1,73 @@
 describe('spacecase main page flow', () => {
   it('should show a grid of all of the spacecrafts', () => {
-
+    cy.fixture('spacecraft.json').as('spacecraft').then((spacecraft) => {
+      cy.intercept('GET', 'https://lldev.thespacedevs.com/2.2.0/spacecraft/?limit=1000&offset=0', {
+        body: spacecraft
+      })
+    })
+      .visit('localhost:3000/spacecraft/spacecraft')
+      .get('.craft-image')
+      .should('have.attr', 'src')
+      .and('equal', 'https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/orbiter_images/mercury_image_20190207032519.jpeg')
   });
-  
+
+  it('should show an error message if the API is down & give the user a way to navigate home', () => {
+    cy.intercept('GET', 'https://lldev.thespacedevs.com/2.2.0/spacecraft/?limit=1000&offset=0', {
+      statusCode: 500,
+      ok: false,
+      body: {}
+    })
+      .visit('localhost:3000/spacecraft/spacecraft')
+      .get('.error-container')
+      .contains('Cannot fetch the data')
+      .get('.home-btn').click()
+      .url().should('contain', '/')
+  });
+
+  it('should show a button that returns the user to the launch page', () => {
+    cy.fixture('spacecraft.json').as('spacecraft').then((spacecraft) => {
+      cy.intercept('GET', 'https://lldev.thespacedevs.com/2.2.0/spacecraft/?limit=1000&offset=0', {
+        body: spacecraft
+      })
+    })
+      .visit('localhost:3000/spacecraft/spacecraft')
+      .get('.home-btn').click()
+      .url().should('contain', '/')
+  });
+
+  it('should be able to search the spacecraft displayed by name', () => {
+    cy.fixture('spacecraft.json').as('spacecraft').then((spacecraft) => {
+      cy.intercept('GET', 'https://lldev.thespacedevs.com/2.2.0/spacecraft/?limit=1000&offset=0', {
+        body: spacecraft
+      })
+    })
+      .visit('localhost:3000/spacecraft/spacecraft')
+      .get('input').type('mer')
+      .get('.main-container').children().should('have.length', '10')
+  });
+
+  it.skip('should show a message if no results match the search query', () => {
+    cy.fixture('spacecraft.json').as('spacecraft').then((spacecraft) => {
+      cy.intercept('GET', 'https://lldev.thespacedevs.com/2.2.0/spacecraft/?limit=1000&offset=0', {
+        body: spacecraft
+      })
+    })
+      .visit('localhost:3000/spacecraft/spacecraft')
+      .get('input').type('bdjaksldbghjksalbfdhjkla')
+      .get('.main-container').children().should('have.length', '1')
+      .get('h2').should('contain', 'No results matched your search.')
+  });
+
+  it('should allow the user to select a spacecraft to see more details', () => {
+    cy.fixture('spacecraft.json').as('spacecraft').then((spacecraft) => {
+      cy.intercept('GET', 'https://lldev.thespacedevs.com/2.2.0/spacecraft/?limit=1000&offset=0', {
+        body: spacecraft
+      })
+    })
+      .visit('localhost:3000/spacecraft/spacecraft')
+      .get('.craft-image-container')
+      .get('a[href="/details/spacecraft/2"]').click()
+      .url().should('contain', '/details/spacecraft/2')
+  });
+
 })
